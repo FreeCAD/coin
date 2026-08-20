fb557b5e35a6340ef668c504b0dc7511c4e5943b
Joao Matos <joao@tritao.eu>
2026-08-20 16:08:43 +0100

    tests: support visible resizable GL contexts

--- testsuite/support/GLTestContext.h
+++ testsuite/support/GLTestContext.h
@@ -31,6 +31,8 @@ struct GLTestContextConfig {
   int minor = 3;
   int width = 64;
   int height = 64;
+  bool visible = false;
+  bool vsync = false;
 };
 
 class GLTestContext {
@@ -45,7 +47,12 @@ public:
   void shutdown();
 
   bool makeCurrent();
+  bool resizeFramebuffer(int width, int height);
   void bindFramebuffer();
+  void present();
+  void pollEvents();
+  bool shouldClose() const;
+  GLFWwindow * window() const { return this->window; }
   std::vector<uint8_t> readPixels() const;
 
   bool isCoreProfile() const { return profile_ == GLTestProfile::Core; }
